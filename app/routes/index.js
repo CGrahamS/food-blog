c04fdc1b0278b6854e69bd49b938b9e8e909a832
Caleb Stevenson and chris martinez <caleb+chris@>
2016-10-26 13:29:50 -0700

    add non-functional delete button that shows dpeening on admin status, pass admin value down

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,13 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return Ember.RSVP.hash({
+      admin: true,
       entries: this.store.findAll('entry')
     });
   },
+
   actions: {
     post2(params){
       var newPost = this.store.createRecord('entry', params);
       newPost.save();
+      this.transitionTo('index');
+    },
+    destroyEntry(entry){
+      entry.destroyRecord();
       this.transitionTo('index');
     }
   }
