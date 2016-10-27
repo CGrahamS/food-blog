32b0ee835049a02893aaf12bfe91d9d01242b264
Caleb Stevenson and chris martinez <caleb+chris@>
2016-10-27 15:02:52 -0700

    add background to panel on hover and pickachu

--- app/models/entry.js
+++ app/models/entry.js
@@ -7,4 +7,10 @@ export default DS.Model.extend({
   date: DS.attr("string"),
   image: DS.attr("string"),
   comments: DS.hasMany('comments', {async: true}),
+  action:{
+    stuff(){
+      alert("hey");
+      return "why hey";
+    }
+  }
 });
